2d1763b356d603d247d682ef64bc919599dd01e1
Kairules55 <krishnakhatri264@gmail.com>
2019-02-23 01:26:50 +0530

    Submission Form

--- WebProgramming/application.py
+++ WebProgramming/application.py
@@ -4,5 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():  
-    name = request.args.get('name','world!')  
-    return render_template("index.html",name=name)
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    game = request.form.get("game")
+
+    if not name or not game:
+        return "failure"
+    return render_template("success.html")    
